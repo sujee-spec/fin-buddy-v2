77af7cc6db57937656be7ee509110a35d75a145c
sujeet <sujeet@infraspec.dev>
2025-09-01 15:38:56 +0530

    workflow

---
+++ .github/scripts/set-gh-role.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+root_dir=$(echo "$1" | cut -d '/' -f 1)
+
+case "$root_dir" in
+  root)
+    ROLE_ARN=${ROOT_GH_ROLE}
+    ;;
+  core)
+    ROLE_ARN=${CORE_GH_ROLE}
+    ;;
+  production)
+    ROLE_ARN=${PRODUCTION_GH_ROLE}
+    ;;
+  staging)
+    ROLE_ARN=${STAGING_GH_ROLE}
+    ;;
+  *)
+    echo "Unknown account $root_dir"
+    exit 1
+    ;;
+esac
+
+echo "Assuming role $ROLE_ARN for account $root_dir"
+echo "role_arn=$ROLE_ARN" >> $GITHUB_OUTPUT
