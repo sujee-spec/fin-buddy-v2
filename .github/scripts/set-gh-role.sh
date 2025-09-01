7dce4c37449d7e3a3dea4151c8610580635dc225
sujeet <sujeet@infraspec.dev>
2025-09-01 18:17:20 +0530

    Refactor: remove unused roles

--- .github/scripts/set-gh-role.sh
+++ .github/scripts/set-gh-role.sh
@@ -3,15 +3,6 @@
 root_dir=$(echo "$1" | cut -d '/' -f 1)
 
 case "$root_dir" in
-  root)
-    ROLE_ARN=${ROOT_GH_ROLE}
-    ;;
-  core)
-    ROLE_ARN=${CORE_GH_ROLE}
-    ;;
-  production)
-    ROLE_ARN=${PRODUCTION_GH_ROLE}
-    ;;
   staging)
     ROLE_ARN=${STAGING_GH_ROLE}
     ;;
--- workload-ou/main.tf
+++
@@ -1,5 +0,0 @@
-module "application_ou" {
-  source                   = "github.com/sujeet32"
-  
-  organizational_unit_name = "Workload"
-}
