c29b8ead8a87a388871841010d531aed7c37f030
sujeet <sujeet@infraspec.dev>
2025-09-01 21:04:19 +0530

    Refactor output.tf

--- root/output.tf
+++ root/output.tf
@@ -2,7 +2,6 @@ output "organization_info" {
   description = "Info about the existing AWS Organization"
   value = {
     id   = data.aws_organizations_organization.this.id
-    
     arn  = data.aws_organizations_organization.this.arn
   }
 }
