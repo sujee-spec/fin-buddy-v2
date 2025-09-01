9e5f540c7641eed1d44467ade8c7ae74a69f5692
sujeet <sujeet@infraspec.dev>
2025-09-01 21:08:23 +0530

    terraform fmt

--- root/output.tf
+++ root/output.tf
@@ -1,7 +1,7 @@
 output "organization_info" {
   description = "Info about the existing AWS Organization"
   value = {
-    id   = data.aws_organizations_organization.this.id
-    arn  = data.aws_organizations_organization.this.arn
+    id  = data.aws_organizations_organization.this.id
+    arn = data.aws_organizations_organization.this.arn
   }
 }
--- root/versions.tf
+++ root/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = "~> 1.13.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
