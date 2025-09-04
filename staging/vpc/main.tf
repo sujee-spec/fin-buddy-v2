f0b47229e66f925440935b104c0b3ded25067bae
sujeet <sujeet@infraspec.dev>
2025-09-04 16:04:17 +0530

    Refactor: Change cidr block for vpc

--- staging/vpc/main.tf
+++ staging/vpc/main.tf
@@ -2,12 +2,12 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "staging-vpc"
-  cidr = "172.31.0.0/16"
+  cidr = "172.30.0.0/16"
 
   azs = ["ap-south-1a", "ap-south-1b"]
 
-  public_subnets  = ["172.31.0.0/20", "172.31.0.0/20"]
-  private_subnets = ["172.31.0.0/20", "172.31.0.0/20"]
+  public_subnets  = ["172.30.1.0/24", "172.30.2.0/24"]
+  private_subnets = ["172.30.3.0/24", "172.30.4.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
